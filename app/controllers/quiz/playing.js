a7c8b77a7c685558a89847f667ef72c20d2782cf
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-01-14 15:36:50 +0100

    styling updates

--- app/controllers/quiz/playing.js
+++ app/controllers/quiz/playing.js
@@ -2,7 +2,5 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 
 export default class PlayingController extends Controller {
-    queryParams = ['difficulty', 'category'];
-    @tracked difficulty = '';
-    @tracked category = '';
+    queryParams = [{ difficulty: { refreshModel: true } }, { category: { refreshModel: true } }];
 }
